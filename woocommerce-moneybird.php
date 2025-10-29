4ccfe6a2fc5bfe7a8a933676f1bfaab59e1177b3
Thomas Schoffelen <thomas@schof.co>
2025-10-29 14:46:29 +0000

    chore: init plugin

---
+++ woocommerce-moneybird.php
@@ -0,0 +1,12 @@
+/*
+* Plugin Name: Sync to Moneybird
+* Description: A WooCommerce plugin to sync orders to Moneybird.
+* Version: 1.0.0
+* Requires at least: 5.2
+* Author: Thomas Schoffelen
+* Author URI: https://includable.com
+* License: MIT
+* Text Domain: woocommerce-moneybird
+* Requires Plugins: woocommerce
+*/
+
